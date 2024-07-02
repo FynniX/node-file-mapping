7960509ff6b8d793f1ab2487e80598462b0cf179
Fynn Haupt <info@fynnhaupt.de>
2024-07-02 22:52:36 +0200

    added enums to index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
 export { BufferReader } from './classes/BufferReader.js';
 export { BufferWriter } from './classes/BufferWriter.js';
 export { FileMapping } from './classes/FileMapping.js';
+export { Endian } from './enums/Endian.js';
+export { VarType } from './enums/VarType.js';
