d0d48d33bd15ebac1f6594bbe9d4c4f77313ca1a
Fynn Haupt <info@fynnhaupt.de>
2024-07-02 21:56:06 +0200

    added rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript'
+import multi from '@rollup/plugin-multi-entry'
+
+export default {
+    input: 'src/**/*.ts',
+    output: [
+        {
+            dir: 'dist',
+            format: 'cjs',
+            preserveModules: true,
+            exports: 'named'
+        },
+        {
+            dir: 'dist',
+            format: 'es',
+            entryFileNames: '[name].mjs',
+            preserveModules: true,
+            exports: 'named'
+        }
+    ],
+    plugins: [
+        typescript(),
+        multi({
+            preserveModules: true
+        })
+    ]
+};
