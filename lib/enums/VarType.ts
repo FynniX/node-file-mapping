77bcbf739b8e9fafb0d7d1caaf61b7d2c16551bc
Fynn Haupt <info@fynnhaupt.de>
2024-07-02 22:51:50 +0200

    added enum for a variable type

---
+++ lib/enums/VarType.ts
@@ -0,0 +1,32 @@
+export enum VarType {
+    char,
+    char16_t,
+    char32_t,
+    wchar_t,
+    unsigned_char,
+
+    short_int,
+    int,
+    long_int,
+    long_long_int,
+
+    unsigned_short_int,
+    unsigned_int,
+    unsigned_long_int,
+    unsigned_long_long_int,
+
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+
+    float,
+    double,
+
+    bool
+}
