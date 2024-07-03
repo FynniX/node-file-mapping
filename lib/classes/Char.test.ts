18fc306b684f961bb17184f3f183d2d894b8e108
Fynn Haupt <info@fynnhaupt.de>
2024-07-03 12:04:24 +0200

    updated Char.test.ts

--- lib/classes/Char.test.ts
+++ lib/classes/Char.test.ts
@@ -1,15 +1,15 @@
 import { describe, expect, test } from "@jest/globals";
 import { Char } from "./Char.js";
 
-describe("Test the char class", () => {
-  test("Validate the char class with valid data", () => {
+describe("Test the Char class", () => {
+  test("Validate the Char class with valid data", () => {
     const randomNumber = Math.floor(Math.random() * 100);
     const char = new Char(randomNumber);
     expect(char.toNumber()).toBe(randomNumber);
     expect(char.toString()).toBe(String.fromCharCode(randomNumber));
   });
 
-  test("Validate the char class with invalid data", () => {
+  test("Validate the Char class with invalid data", () => {
     expect(() => new Char(65536)).toThrow(Error);
   });
 });
